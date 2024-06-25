e141d6a813abfc3fd7dfea0f24a714b95161726e
Steven Cardenas V <stevencardenasvela@gmail.com>
2024-06-24 20:24:55 -0500

    agregando dashboard inicial

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { LoginRegisterServiceService } from './service/login-register-service.service';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -10,8 +9,10 @@ import { SpinnerInterceptor } from './service/spinner.interceptor';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
 import { IMAGE_CONFIG } from '@angular/common';
 import { DatePipe } from '@angular/common';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
+    provideCharts(withDefaultRegisterables()),
     DatePipe,
     provideHttpClient(withInterceptors([authInterceptor,SpinnerInterceptor])), provideAnimationsAsync(),
     provideOAuthClient(),
--- src/app/components/dashboards/main-dashboard/main-dashboard.component.ts
+++ src/app/components/dashboards/main-dashboard/main-dashboard.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
-
+import { ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 @Component({
   selector: 'app-main-dashboard',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './main-dashboard.component.html',
   styleUrl: './main-dashboard.component.css'
 })
 export class MainDashboardComponent {
+
+  salesData: ChartData<'pie'> = {
+    labels: ['Total Cobrado', 'Pendiente de Cobro'],
+    datasets: [{
+      data: [23, 5],
+      backgroundColor: ['#4CAF50', '#FF5252'],
+    }],
+  };
+
+  salesLabels = ['Total Cobrado', 'Pendiente de Cobro'];
+
+  invoicesData: ChartData<'doughnut'> = {
+    labels: ['Total Cobrado', 'Pendiente de Cobro'],
+    datasets: [{
+      data: [269.50, 23.45],
+      backgroundColor: ['#4CAF50', '#FF5252'],
+    }],
+  };
+
+  invoicesLabels = ['Total Cobrado', 'Pendiente de Cobro'];
+
+  totalsData: ChartData<'bar'> = {
+    labels: ['16d', '17d', '18d', '19d', '20d', '21d', '22d', '23d'],
+    datasets: [
+      { label: 'Total notas de venta', data: [1, 4, 16, 23, 0, 0, 0, 0], backgroundColor: '#FF5252' },
+      { label: 'Total comprobantes', data: [0, 3, 1, 269.50, 0, 0, 0, 0], backgroundColor: '#4CAF50' },
+      { label: 'Total', data: [0, 0, 0, 292.50, 0, 123.4, 0, 0], backgroundColor: '#9E9E9E' }
+    ],
+  };
+
+  totalsLabels = ['16d', '17d', '18d', '19d', '20d', '21d', '22d', '23d'];
+
+
+
+  purchasesData: ChartData<'bar'> = {
+    labels: ['16d', '17d', '18d', '19d', '20d', '21d', '22d', '23d'],
+    datasets: [
+      { label: 'Total notas de compras', data: [1, 4, 16, 23, 0, 0, 0, 0], backgroundColor: '#FF5252' },
+      { label: 'Total comprobantes', data: [0, 3, 1, 269.50, 0, 0, 0, 0], backgroundColor: '#4CAF50' },
+      { label: 'Total', data: [0, 0, 0, 292.50, 0, 123.4, 0, 0], backgroundColor: '#9E9E9E' }
+    ],
+  };
+
+  purchasesLabels= ['16d', '17d', '18d', '19d', '20d', '21d', '22d', '23d'];
+
 
 }
